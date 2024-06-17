c9fd1a3a4f6e6cd8c8667eee526f37453ebba7fb
JordenHuang <jorden90573@gmail.com>
2024-06-17 23:47:17 +0800

    Add 'jump to file' functionality

--- lua/como/buffer.lua
+++ lua/como/buffer.lua
@@ -1,6 +1,16 @@
 -- TODO: set auto scroll when at the last line, else don't scroll
 local M = {}
 
+local mh = require('como.matcher')
+local uv = vim.uv or vim.loop
+
+local Pos = {
+    name = 1,
+    start_col = 2,
+    end_col = 3,
+    data = 4
+}
+
 M.buf = false
 
 M.if_buf_is_valid = function(buf_to_check)
@@ -39,13 +49,15 @@ M.create_buf = function()
     local buf = vim.api.nvim_create_buf(false, false)
     -- Set some options for the buffer
     vim.api.nvim_buf_set_name(buf, "Compilation mode")
-    vim.api.nvim_buf_set_option(buf, 'buftype', 'nowrite')
+    vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
     vim.api.nvim_buf_set_option(buf, 'modifiable', false)
     -- vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
     vim.api.nvim_buf_set_option(buf, 'swapfile', false)
     vim.api.nvim_buf_set_option(buf, 'buflisted', false)
 
     vim.api.nvim_buf_set_keymap(buf, 'n', 'q', ":q<CR>", {silent=true, noremap=true, desc="close como window"})
+    -- Jump to file when hit enter
+    vim.api.nvim_buf_set_keymap(buf, 'n', "<CR>", ":lua require('como.buffer').jump_to_file()<CR>", {silent=true, noremap=true, desc="como jump to file"})
 
     M.buf = buf
     return buf
@@ -68,6 +80,89 @@ M.buf_open = function()
         vim.api.nvim_win_set_buf(win, buf)
     end
     return buf
+end
+
+-- Jump to the file
+M.jump_to_file = function()
+    local line = vim.api.nvim_get_current_line()
+    -- print('current line: ' .. line)
+
+    local result = mh.parse_line(line)
+    if result == nil then
+        return nil
+    end
+
+    -- Variables with default value nil
+    local file_path, lnum, col
+    -- Loop through the parts in the line, to get the filename, lnum (and probrobly col)
+    for _, part in ipairs(result.parts) do
+        -- Get the filename
+        if part[Pos.name] == "filename" then
+            -- print("The 'only' filename: ")
+            -- print(vim.fn.fnamemodify(part[Pos.data], ":t"))
+
+            file_path = vim.fn.fnamemodify(part[Pos.data], ":p")
+            -- Try to find the file with its full path
+            local ok, err = uv.fs_stat(file_path)
+            if not ok then
+                print("Error to find file:")
+                print(err)
+                return nil
+                -- else
+                --     print(file_path)
+                --     print("no problem")
+            end
+        end
+
+        -- Get the line number
+        if part[Pos.name] == "lnum" then
+            lnum = tonumber(part[Pos.data])
+        end
+
+        -- Get the column number
+        if part[Pos.name] == "col" then
+            col = tonumber(part[Pos.data])
+        end
+    end
+
+    if file_path ~= nil and lnum ~= nil then
+        M.open_file_and_set_cursor(file_path, lnum, col)
+    else
+        return nil
+    end
+end
+
+-- By Chat-GPT
+M.open_file_and_set_cursor = function(file_path, row, col)
+    local is_file_open = false
+    local win_id = nil
+
+    -- Iterate over all windows to check if the file is already open
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        local buf_name = vim.api.nvim_buf_get_name(buf)
+        if buf_name == file_path then
+            is_file_open = true
+            win_id = win
+            break
+        end
+    end
+
+    if is_file_open then
+        -- Focus the window where the file is open
+        vim.api.nvim_set_current_win(win_id)
+    else
+        -- Open a new window below and open the file
+        vim.cmd('belowright split ' .. file_path)
+    end
+
+    -- Set the cursor position
+    if col then
+        -- let col minus 1 because it's 0-indexed
+        vim.api.nvim_win_set_cursor(0, {row, col-1})
+    else
+        vim.api.nvim_win_set_cursor(0, {row, 0})
+    end
 end
 
 return M
--- lua/como/matcher.lua
+++ lua/como/matcher.lua
@@ -3,58 +3,56 @@ local M = {}
 M.matcher_set = {
     gcc = {
         pattern = "(%S+):(%d+):(%d+): (%S+): (.+)",
-        [1] = "filename",
-        [2] = "lnum",
-        [3] = "col",
-        [4] = "qtype",
-        [5] = "message"
+        parts = {
+            [1] = "filename",
+            [2] = "lnum",
+            [3] = "col",
+            [4] = "qtype",
+            [5] = "message"
+        }
     },
     python = {
-        pattern = "  File \"(%S+)\", (line %d+), (.+)",
-        [1] = "filename",
-        [2] = "lnum",
-        [3] = "message"
+        pattern = "  File \"(%S+)\", line (%d+), (.+)",
+        parts = {
+            "filename",
+            "lnum",
+            "message"
+        }
     },
 }
 
 M.parse_line = function(line)
     for mname, matcher in pairs(M.matcher_set) do
         local res = {}
-        local parts = {string.match(line, matcher.pattern)}
+        local values = { string.match(line, matcher.pattern) }
         -- for a, b in ipairs(parts) do
         --     print(a, b)
         -- end
 
-        if #parts ~= 0 then
-            res.mname = mname
-            res.mpattern = matcher.pattern
-            -- for i = 1, #parts do
-            --     -- print(i, parts[i], matcher[i])
-            --     res[matcher[i]] = parts[i]
-            -- end
-            -- local temp = M.calc_position(matcher, parts, line)
-            -- print(vim.inspect(temp))
-            res.parts = M.calc_position(matcher, parts, line)
+        if #values ~= 0 then
+            -- res.mname = mname
+            -- res.mpattern = matcher.pattern
+            res.parts = M.calc_position(matcher.parts, values, line)
             return res
         end
     end
     return nil
 end
 
-M.calc_position = function(matcher, parts, line)
+M.calc_position = function(parts, values, line)
     local res = {}
     local start_col, end_col
     local next_start = 1
-    for i = 1, #parts do
+    for i = 1, #values do
         -- Needs to give 'plain' argument, or some operator in parts[i] will be treated as 'magic'. See :h string.find()
-        start_col, end_col = string.find(line, parts[i], next_start, true)
+        start_col, end_col = string.find(line, values[i], next_start, true)
 
         start_col = tonumber(start_col)
         end_col = tonumber(end_col)
         next_start = end_col + 1
 
-        -- matcher[i] is the part's name, like filename, lnum .etc
-        res[i] = { matcher[i], start_col, end_col, parts[i] }
+        -- parts[i] is the part's name, like filename, lnum .etc
+        res[i] = { parts[i], start_col, end_col, values[i] }
     end
     return res
 end
